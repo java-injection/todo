12ab25c5555433b919e26bfa0d23d1868922b70b
sommovir <luca.coraci@gmail.com>
2023-12-05 19:13:16 +0100

    test connection to clickup

--- src/main/java/it/ji/drivers/ClickUpAPI.java
+++ src/main/java/it/ji/drivers/ClickUpAPI.java
@@ -1,13 +1,21 @@
 package it.ji.drivers;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 import java.util.List;
 
 public class ClickUpAPI {
 
     private static ClickUpAPI _instance = null;
+    private static final String API_TOKEN = "APY_TOKEN";
+    private static final String BASE_URL = "https://api.clickup.com/api/v2";
 
+    private final OkHttpClient client;
     private ClickUpAPI(){
-
+        client = new OkHttpClient();
     }
 
     public static ClickUpAPI getInstance(){
@@ -19,6 +27,18 @@ public class ClickUpAPI {
 
     public List<String> getStatuses(){
         return List.of("todo", "in progress", "done");
+    }
+
+
+    public String getTasks(String listId) throws IOException {
+        Request request = new Request.Builder()
+                .url(BASE_URL + "/list/" + listId + "/task")
+                .addHeader("Authorization", API_TOKEN)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
 
 
